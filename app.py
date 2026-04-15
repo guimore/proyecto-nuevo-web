47fb7630d8b96d9055b6b5f56753cf6ca0a76fa2
Guido Mora <mora.guido@gmail.com>
2026-04-14 23:33:45 -0300

    Ahora sí: usando render_template en la rama main

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template  # <--- Importante para leer el HTML
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Esto le dice a Flask: "Buscá el index.html adentro de la carpeta templates"
+    # Esta línea busca tu index.html dentro de la carpeta templates
     return render_template('index.html')
 
 if __name__ == "__main__":
-    # Railway usa esta variable para asignar el puerto
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)

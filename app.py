e87c3817440f437bd105e892e9aaf718002cbf6a
Guido Mora <mora.guido@gmail.com>
2026-04-14 23:51:46 -0300

    Versión final probada localmente - Todo OK

--- app.py
+++ app.py
@@ -3,11 +3,27 @@ import os
 
 app = Flask(__name__)
 
+# Ruta para el Inicio (index.html)
 @app.route('/')
 def home():
-    # Esta línea busca tu index.html dentro de la carpeta templates
     return render_template('index.html')
 
+# Ruta para Nosotros
+@app.route('/nosotros')
+def nosotros():
+    return render_template('nosotros.html')
+
+# Ruta para Galería
+@app.route('/galeria')
+def galeria():
+    return render_template('galeria.html')
+
+# Ruta para Contacto
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
 if __name__ == "__main__":
+    # Esto es vital para que Railway asigne el puerto correctamente
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
